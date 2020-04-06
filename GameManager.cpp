3330b86f284beb6cf7268cb96334154658f868b2
Martin Heistad <ma-heist@hotmail.com>
2020-04-06 20:43:34 +0200

    - Added potential solution for freezing game at the start while music is playing - Added possible solution for Pausing game.

--- GameManager.cpp
+++ GameManager.cpp
@@ -42,8 +42,15 @@ int GameManager::startGame() {
             pacman.setHealth();
         }
         if(quitter[SDL_SCANCODE_ESCAPE]){
-            pacman.setHealth();
+            break;
         }
+//        if(quitter[SDL_SCANCODE_P]){
+//            if(game_state == 2){
+//                game_state = 3;
+//            } else if(game_state == 3) { Potentiell løsning for å pause programmet, problemet er at når man trykker på P så går den insta inn i gamestate 1 igjen :o - Martin
+//                game_state = 1;
+//        }
+//        }
             if(game_state == 1)
             {
 
@@ -52,13 +59,18 @@ int GameManager::startGame() {
                 if (openingSound == nullptr) {
                     printf("Failed to load scratch sound effect! SDL_mixer Error: %s\n", Mix_GetError());
                 }
+
                 Mix_Volume(-1, 5);
-                Mix_PlayChannel(-1, openingSound, 0);
-                render();
+                Mix_PlayChannel(1, openingSound, 0);
+
+               // while(!(Mix_Playing(1) == 0)){
+                    //  Denne While loopen fryser spillet på riktig måte men Spøkelset blir ikke med :o -Martin
+               //}
                 game_state = 2;
             }
             if(game_state == 2)
             {
+
                 calculateDeltaTime();
                 pacman.checkMovementInput(level);
                 pacman.moveCharacter(level);
@@ -77,6 +89,9 @@ int GameManager::startGame() {
                 }
                 if (counter == 2){
                     break;
+            }
+            if(game_state == 3){
+                // Do fuck all
             }
         }
     }
